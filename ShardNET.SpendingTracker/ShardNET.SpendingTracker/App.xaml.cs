ac9fda99c767cc9661552020d65e982fec80a1b8
Dev-L-OPs <dev.el.ops@outlook.com>
2023-09-20 16:19:32 +0200

    spending tracker app project init

---
+++ ShardNET.SpendingTracker/ShardNET.SpendingTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShardNET.SpendingTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShardNET.SpendingTracker/ShardNET.SpendingTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShardNET.SpendingTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
